282e2d3c4685b8a142b90881bf43b7bde201cf13
prabhu-rao_bveng <prabhu.rao@bazaarvoice.com>
2024-07-01 06:31:19 +0530

    bloom filter

---
+++ src/main/java/web/consistent/hashing/BloomFilters.java
@@ -0,0 +1,37 @@
+package web.consistent.hashing;
+
+// Java program to implement
+// Bloom Filter using Guava Library
+
+import java.nio.charset.Charset;
+import com.google.common.hash.BloomFilter;
+import com.google.common.hash.Funnels;
+
+public class BloomFilters {
+    public static void main(String[] args)
+    {
+
+        // Create a Bloom Filter instance
+        BloomFilter<String> blackListedIps
+                = BloomFilter.create(
+                Funnels.stringFunnel(
+                        Charset.forName("UTF-8")),
+                10000);
+
+        // Add the data sets
+        blackListedIps.put("192.170.0.1");
+        blackListedIps.put("75.245.10.1");
+        blackListedIps.put("10.125.22.20");
+
+        // Test the bloom filter
+        System.out.println(
+                blackListedIps
+                        .mightContain(
+                                "75.245.10.1"));
+        System.out.println(
+                blackListedIps
+                        .mightContain(
+                                "101.125.20.22"));
+    }
+}
+
